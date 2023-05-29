69f43753bdf50e56a772c5c4bf42a24c56d0e5ee
Bogdan <romanovbogdan4@gmail.com>
2023-05-29 21:44:35 +0300

    Add parser for webpage after 04/06/2019

--- genius_parser.py
+++ genius_parser.py
@@ -15,12 +15,12 @@ class GeniusScraper:
     def __init__(self, start_date: datetime, end_date: datetime):
         self.start_date = start_date
         self.end_date = end_date
-        self.links = []
+        self.dates_links = []
         self.session = self.setting_connection()
 
         self.song_info_logger = logging.getLogger('song_info_logger')
         self.song_info_logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler('song_info_log.txt', 'w')
+        handler = logging.FileHandler('song_info_log.txt')
         formatter = logging.Formatter('%(asctime)s - %(message)s')
         handler.setFormatter(formatter)
         self.song_info_logger.addHandler(handler)
@@ -28,15 +28,33 @@ class GeniusScraper:
         # Set up logger for parsing
         self.lyrics_logger = logging.getLogger('lyrics_logger')
         self.lyrics_logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler('song_lyrics_log.txt', 'w')
+        handler = logging.FileHandler('song_lyrics_log.txt')
         handler.setFormatter(formatter)
         self.lyrics_logger.addHandler(handler)
+
 
     @staticmethod
     def get_link(date: datetime):
         date_str = date.strftime('%Y%m%d')
         webarchive = f"http://web.archive.org/web/{date_str}/https://genius.com/"
         return webarchive
+
+    @staticmethod
+    def log(filename, page_url):
+        logfile = f"{filename}.txt"
+        timestamp_format = '%Y-%h-%d-%H:%M:%S'
+        now = datetime.now()
+        timestamp = now.strftime(timestamp_format)
+        with open(logfile, "a") as f:
+            f.write(timestamp + ', ' + f'scraping data from the link: {page_url}' + '\n'
+                    )
+
+    @staticmethod
+    def clear_log(filename):
+        logfile = f"{filename}.txt"
+        with open(logfile, "w") as f:
+            pass
+
 
     @staticmethod
     def has_lyrics_class(tag):
@@ -46,6 +64,7 @@ class GeniusScraper:
                     or value == 'lyrics_ctrl.editing' or value == 'song_body-lyrics':
                 return True
         return False
+
 
     @staticmethod
     def setting_connection():
@@ -62,6 +81,12 @@ class GeniusScraper:
 
     @staticmethod
     def parser_2018(soup, output):
+        """
+        works until 2019 6 3, after that need to switch to another parser
+        :param soup:
+        :param output:
+        :return:
+        """
         rank = soup.find_all("div", ["chart_row-number_container chart_row-number_container--large"])
         rank = re.findall('(?<=<span\>)(.*?)(?=</span>)', str(rank))
 
@@ -87,30 +112,60 @@ class GeniusScraper:
             'lyrics_links': lyrics_links
         })
 
+
+    @staticmethod
+    def parser_2019(soup, output):
+        rank = soup.find_all("div", ["ChartItemdesktop__Rank-sc-3bmioe-1"])
+        rank = re.findall('(?<="\>)(.*?)(?=</div>)', str(rank))
+
+        song_name = soup.find_all("div", ["ChartSongdesktop__Title-sc-18658hh-3"])
+        song_name = re.findall('(?<="\>)(.*?)(?=</div>)', str(song_name))
+
+        artist_name = soup.find_all("h4", ["ChartSongdesktop__Artist-sc-18658hh-5"])
+        artist_name = re.findall('(?<="\>)(.*?)(?=</h4>)', str(artist_name))
+
+        lyrics = soup.find_all("a", ["PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 izVEsw",
+                                        "PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk"])
+        lyrics_links = re.findall('(?<=href=")(.*?)(?=">)', str(lyrics))
+
+        output.append({"ranks": rank,
+                     "song_titles": song_name,
+                     "artists": artist_name,
+                     "lyrics_links": lyrics_links})
+
+
     @staticmethod
     def unique_bit(link):
         parsed_url = urlparse(link)
         path_parts = parsed_url.path.split('/')
         return path_parts[6]
 
+
     def generate_links(self):
         current_date = self.start_date
         while current_date <= self.end_date:
-            self.links.append(self.get_link(current_date))
+            self.dates_links.append([current_date, self.get_link(current_date)])
             current_date += timedelta(days=1)
+
 
     def collect_data(self):
         self.generate_links()
         output = []
+        self.clear_log('song_info_log')
 
-        for link in self.links:
+        for date, link in self.dates_links:
             time.sleep(random.randint(2, 4))
 
-            self.song_info_logger.info(f'Parsing data from {link}')
+            self.log('song_info_log', link)
 
             r = self.setting_connection().get(link)
             soup = BeautifulSoup(r.content, "html.parser")
-            self.parser_2018(soup, output)
+
+            if date > datetime(2019, 6, 3):
+                self.parser_2019(soup, output)
+            else:
+                self.parser_2018(soup, output)
+
 
         song_info_df = pd.DataFrame(output)
         song_info_df = song_info_df.explode(['ranks',
@@ -119,18 +174,20 @@ class GeniusScraper:
                                              'lyrics_links']).reset_index(drop=True)
         return song_info_df
 
+
     def collect_lyrics(self, df):
         unique_links = {}
         lyrics_list = []
+        self.clear_log('song_lyrics_log')
 
         for lyrics_link in df.lyrics_links:
             unique_linkpart = self.unique_bit(lyrics_link)
             if unique_linkpart in unique_links.values():
                 continue
             else:
-                self.lyrics_logger.info(f'Scraping lyrics from the {lyrics_link}')
+                self.log('song_lyrics_log', lyrics_link)
                 unique_links[unique_linkpart] = unique_linkpart
-                r = requests.get(lyrics_link)
+                r = self.setting_connection().get(lyrics_link)
                 soup = BeautifulSoup(r.content, "html.parser")
                 lyrics = soup.find(self.has_lyrics_class).get_text()
                 lyrics_list.append({"lyrics_links": lyrics_link,
@@ -141,9 +198,10 @@ class GeniusScraper:
         return lyrics_df
 
 
-start_date = datetime(2019, 1, 1)
-end_date = datetime(2019, 1, 10)
+start_date = datetime(2019, 6, 1)
+end_date = datetime(2019, 6, 10)
 scraper = GeniusScraper(start_date, end_date)
 song_info_df = scraper.collect_data()
 lyrics_df = scraper.collect_lyrics(song_info_df)
-# pd.merge(song_info_df, lyrics_df, on='lyrics_links')
+
+info_lyrics = pd.merge(song_info_df, lyrics_df, on='lyrics_links')
