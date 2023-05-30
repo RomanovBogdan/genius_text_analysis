64016cdf04f8fea5648edb99003b4bec9d91eab8
Bogdan <romanovbogdan4@gmail.com>
2023-05-30 16:10:53 +0300

    Combine two separate parsers to one

--- genius_parser.py
+++ genius_parser.py
@@ -1,6 +1,6 @@
 import random
 import pandas as pd
-import time
+from time import sleep
 import re
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
@@ -16,14 +16,14 @@ class GeniusScraper:
         self.end_date = end_date
         self.dates_links = []
         self.unique_lyrics_links = {}
-        self.session = self.setting_connection()
+        self.session = self.set_connection()
 
 
     @staticmethod
     def get_link(date: datetime):
         date_str = date.strftime('%Y%m%d')
-        webarchive = f"http://web.archive.org/web/{date_str}/https://genius.com/"
-        return webarchive
+        return f"http://web.archive.org/web/{date_str}/https://genius.com/"
+
 
     @staticmethod
     def log(filename, message:str, page_url):
@@ -32,8 +32,8 @@ class GeniusScraper:
         now = datetime.now()
         timestamp = now.strftime(timestamp_format)
         with open(logfile, "a") as f:
-            f.write(timestamp + ', ' + f'{message}' + ':' + f'{page_url}' + '\n'
-                    )
+            f.write(timestamp + ', ' + f'{message}' + ': ' + f'{page_url}' + '\n')
+
 
     @staticmethod
     def clear_log(filename):
@@ -53,7 +53,7 @@ class GeniusScraper:
 
 
     @staticmethod
-    def setting_connection():
+    def set_connection():
         session = requests.Session()
 
         retry = Retry(connect=3, backoff_factor=0.5)
@@ -73,77 +73,50 @@ class GeniusScraper:
         else:
             self.log('songs_log', 'collecting lyrics', lyrics_link)
             self.unique_lyrics_links[unique_linkpart] = unique_linkpart
-            r = self.setting_connection().get(lyrics_link)
+            r = self.set_connection().get(lyrics_link)
             soup = BeautifulSoup(r.content, "html.parser")
+
             lyrics_list.append(soup.find(self.has_lyrics_class).get_text())
-            time.sleep(random.randint(2, 4))
+
+            sleep(random.randint(2, 4))
         return lyrics_list
 
 
-    def parser_2018(self, soup, output):
-        """
-        works until 2019 6 3, after that need to switch to another parser
-        """
+    def get_element_content(self, soup, tag:str, element_class:str, str_before:str, str_after:str):
+        element_content = soup.find_all(tag, [element_class])
+        return re.findall(f'(?<={str_before})(.*?)(?={str_after})', str(element_content))
+
+
+    def parse_lyrics(self, soup, output):
         lyrics_list = []
 
-        rank = soup.find_all("div", ["chart_row-number_container chart_row-number_container--large"])
-        rank = re.findall('(?<=<span\>)(.*?)(?=</span>)', str(rank))
-
-        song_name_3 = soup.find_all("div", ["chart_row-two_line_title_and_artist-title"])
-        song_name_separate_3 = re.findall('(?<="\>)(.*?)(?=</div>)', str(song_name_3))
-
-        song_name_7 = soup.find_all("div", ["chart_row-content"])
-        song_name_separate_7 = re.findall('(?<="\>)(.*?)(?= by)', str(song_name_7))
-
-        artist_name_3 = soup.find_all("div", ["chart_row-two_line_title_and_artist"])
-        artist_name_separate_3 = re.findall('(?<=      )(.*?)(?=\n)', str(artist_name_3))
-
-        artist_name_7 = soup.find_all("div", ["chart_row-content"])
-        artist_name_separate_7 = re.findall('(?<=by )(.*?)(?=</div>)', str(artist_name_7))
-
-        lyrics_links = soup.find_all("a", ["chart_row"])
-        lyrics_links = re.findall('(?<=href=")(.*?)(?=">)', str(lyrics_links))
+        rank = self.get_element_content(soup, 'div', "chart_row-number_container chart_row-number_container--large",
+                                 '<span\>', '</span>')
+        song_name_3 = self.get_element_content(soup, 'div', "chart_row-two_line_title_and_artist-title",
+                                               '"\>', '</div>')
+        song_name_7 = self.get_element_content(soup, 'div', "chart_row-content",
+                                               '"\>', ' by')
+        artist_name_3 = self.get_element_content(soup, 'div', "chart_row-two_line_title_and_artist",
+                                               '      ', '\n')
+        artist_name_7 = self.get_element_content(soup, 'div', "chart_row-content",
+                                               'by ', '</div>')
+        lyrics_links = self.get_element_content(soup, 'a', 'chart_row', 'href="', '">')
 
         for lyrics_link in lyrics_links:
             lyrics_list = self.collect_lyrics(lyrics_link, lyrics_list)
 
-
         output.append({
             'ranks': rank,
-            'song_titles': song_name_separate_3 + song_name_separate_7,
-            'artists': artist_name_separate_3 + artist_name_separate_7,
+            'song_titles': song_name_3 + song_name_7,
+            'artists': artist_name_3 + artist_name_7,
             'lyrics_links': lyrics_links,
             'lyrics': lyrics_list
         })
 
 
-
-    @staticmethod
-    def parser_2019(soup, output):
-        rank = soup.find_all("div", ["ChartItemdesktop__Rank-sc-3bmioe-1"])
-        rank = re.findall('(?<="\>)(.*?)(?=</div>)', str(rank))
-
-        song_name = soup.find_all("div", ["ChartSongdesktop__Title-sc-18658hh-3"])
-        song_name = re.findall('(?<="\>)(.*?)(?=</div>)', str(song_name))
-
-        artist_name = soup.find_all("h4", ["ChartSongdesktop__Artist-sc-18658hh-5"])
-        artist_name = re.findall('(?<="\>)(.*?)(?=</h4>)', str(artist_name))
-
-        lyrics = soup.find_all("a", ["PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 izVEsw",
-                                        "PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk"])
-        lyrics_links = re.findall('(?<=href=")(.*?)(?=">)', str(lyrics))
-
-        output.append({"ranks": rank,
-                     "song_titles": song_name,
-                     "artists": artist_name,
-                     "lyrics_links": lyrics_links})
-
-
     @staticmethod
     def unique_bit(link):
-        parsed_url = urlparse(link)
-        path_parts = parsed_url.path.split('/')
-        return path_parts[6]
+        return urlparse(link).path.split('/')[6]
 
 
     def generate_links(self):
@@ -156,14 +129,15 @@ class GeniusScraper:
     def collect_data(self):
         self.generate_links()
         output = []
+
         self.clear_log('songs_log')
 
         for date, link in self.dates_links:
-            time.sleep(random.randint(2, 4))
+            sleep(random.randint(2, 4))
 
             self.log('songs_log', 'collecting song data', link)
 
-            r = self.setting_connection().get(link)
+            r = self.set_connection().get(link)
             soup = BeautifulSoup(r.content, "html.parser")
 
             if date > datetime(2019, 6, 3):
@@ -183,6 +157,6 @@ class GeniusScraper:
 
 
 start_date = datetime(2018, 1, 1)
-end_date = datetime(2018, 3, 1)
+end_date = datetime(2018, 2, 1)
 scraper = GeniusScraper(start_date, end_date)
 song_info_df = scraper.collect_data()
