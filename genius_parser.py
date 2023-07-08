3f106a309d0593448f55ebdd0c4f89d291acfbc3
Bogdan <romanovbogdan4@gmail.com>
2023-07-08 19:37:54 +0300

    Refactor parser

--- genius_parser.py
+++ genius_parser.py
@@ -8,15 +8,20 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from urllib.parse import urlparse
+import json
 
 
 class GeniusScraper:
-    def __init__(self, start_date: datetime, end_date: datetime):
+    def __init__(self, config_path: str, start_date: datetime, end_date: datetime):
+        self.config_path = config_path
         self.start_date = start_date
         self.end_date = end_date
         self.dates_links = []
         self.unique_lyrics_links = {}
         self.session = self.set_connection()
+        self.lyrics_list = []
+        self.load_config()
+
 
 
     @staticmethod
@@ -38,7 +43,7 @@ class GeniusScraper:
     @staticmethod
     def clear_log(filename):
         logfile = f"{filename}.txt"
-        with open(logfile, "w") as f:
+        with open(logfile, "w"):
             pass
 
 
@@ -69,7 +74,6 @@ class GeniusScraper:
         unique_linkpart = self.unique_bit(lyrics_link)
         if unique_linkpart in self.unique_lyrics_links.values():
             lyrics_list.append('We already listened to this one')
-            pass
         else:
             self.log('songs_log', 'collecting lyrics', lyrics_link)
             self.unique_lyrics_links[unique_linkpart] = unique_linkpart
@@ -87,48 +91,65 @@ class GeniusScraper:
         return re.findall(f'(?<={str_before})(.*?)(?={str_after})', str(element_content))
 
 
-    def parse_lyrics(self, soup, output, date):
-        rank, song_name, artist_name, lyrics_links, lyrics_list = [], [], [], [], []
-
-        if date > datetime(2019, 6, 3):
-            rank = self.get_element_content(soup, "div", ["ChartItemdesktop__Rank-sc-3bmioe-1"],
-                                            '"\>', '</div>')
-            song_name = self.get_element_content(soup, "div", ["ChartSongdesktop__Title-sc-18658hh-3"],
-                                            '"\>', '</div>')
-            artist_name = self.get_element_content(soup, "h4", ["ChartSongdesktop__Artist-sc-18658hh-5"],
-                                            '"\>', '</h4>')
-            lyrics_links = self.get_element_content(soup, "a",
-                                                    ["PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 izVEsw",
-                                                     "PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk"],
-                                                    'href="', '">')
-            for lyrics_link in lyrics_links:
-                lyrics_list = self.collect_lyrics(lyrics_link, lyrics_list)
-
-        else:
-            rank = self.get_element_content(soup, 'div', ["chart_row-number_container chart_row-number_container--large"],
+    def parse_web_page_old_version(self, soup):
+        rank = self.get_element_content(soup, 'div', ["chart_row-number_container chart_row-number_container--large"],
                                      '<span\>', '</span>')
-            song_name_3 = self.get_element_content(soup, 'div', ["chart_row-two_line_title_and_artist-title"],
+        song_name_3 = self.get_element_content(soup, 'div', ["chart_row-two_line_title_and_artist-title"],
                                                    '"\>', '</div>')
-            song_name_7 = self.get_element_content(soup, 'div', ["chart_row-content"],
+        song_name_7 = self.get_element_content(soup, 'div', ["chart_row-content"],
                                                    '"\>', ' by')
-            song_name = song_name_3 + song_name_7
-            artist_name_3 = self.get_element_content(soup, 'div', ["chart_row-two_line_title_and_artist"],
+        song_name = song_name_3 + song_name_7
+        artist_name_3 = self.get_element_content(soup, 'div', ["chart_row-two_line_title_and_artist"],
                                                    '      ', '\n')
-            artist_name_7 = self.get_element_content(soup, 'div', ["chart_row-content"],
+        artist_name_7 = self.get_element_content(soup, 'div', ["chart_row-content"],
                                                    'by ', '</div>')
-            artist_name = artist_name_3 + artist_name_7
-            lyrics_links = self.get_element_content(soup, 'a', ['chart_row'], 'href="', '">')
+        artist_name = artist_name_3 + artist_name_7
+        lyrics_links = self.get_element_content(soup, 'a', ['chart_row'], 'href="', '">')
 
-            for lyrics_link in lyrics_links:
-                lyrics_list = self.collect_lyrics(lyrics_link, lyrics_list)
-
-        output.append({
+        lyrics_list = []
+        for lyrics_link in lyrics_links:
+            lyrics_list = self.collect_lyrics(lyrics_link, lyrics_list)
+        return {
             'ranks': rank,
             'song_titles': song_name,
             'artists': artist_name,
             'lyrics_links': lyrics_links,
             'lyrics': lyrics_list
-        })
+            }
+
+    def parse_web_page_new_version(self, soup):
+        rank = self.get_element_content(soup, "div", ["ChartItemdesktop__Rank-sc-3bmioe-1"],
+                                        '"\>', '</div>')
+        song_name = self.get_element_content(soup, "div", ["ChartSongdesktop__Title-sc-18658hh-3"],
+                                             '"\>', '</div>')
+        artist_name = self.get_element_content(soup, "h4", ["ChartSongdesktop__Artist-sc-18658hh-5"],
+                                               '"\>', '</h4>')
+        lyrics_links = self.get_element_content(soup, "a",
+                                                ["PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 izVEsw",
+                                                 "PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk"],
+                                                'href="', '">')
+        lyrics_list = []
+        for lyrics_link in lyrics_links:
+            lyrics_list = self.collect_lyrics(lyrics_link, lyrics_list)
+
+        return {
+            'ranks': rank,
+            'song_titles': song_name,
+            'artists': artist_name,
+            'lyrics_links': lyrics_links,
+            'lyrics': lyrics_list
+        }
+
+
+    def parse_lyrics(self, soup, output, date):
+        if date <= datetime(2019, 6, 3):
+            print('Using old version')
+            data = self.parse_web_page_old_version(soup)
+        else:
+            print('Using new version')
+            data = self.parse_web_page_new_version(soup)
+        output = pd.concat([output, pd.DataFrame(data)], ignore_index=True)
+        return output
 
     @staticmethod
     def unique_bit(link):
@@ -141,34 +162,47 @@ class GeniusScraper:
             self.dates_links.append([current_date, self.get_link(current_date)])
             current_date += timedelta(days=1)
 
+    def load_config(self):
+        with open(self.config_path) as cfg:
+            self.config = json.load(cfg)
+
+    def save_config(self, iteration):
+        self.config['iteration'] = iteration
+        with open('./config.json', 'w', encoding='utf-8') as f:
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+
 
     def collect_data(self):
         self.generate_links()
-        output = []
+
+        if self.config['iteration'] != 0:
+            output = pd.read_csv('filename.csv')
+            iteration = self.config['iteration']
+        else:
+            output = pd.DataFrame()
+            iteration = 0
 
         self.clear_log('songs_log')
 
-        for date, link in self.dates_links:
-            sleep(random.randint(2, 4))
+        try:
+            for date, link in self.dates_links:
+                sleep(random.randint(2, 4))
 
-            self.log('songs_log', 'collecting song data', link)
+                self.log('songs_log', 'collecting song data', link)
 
-            r = self.set_connection().get(link)
-            soup = BeautifulSoup(r.content, "html.parser")
+                r = self.set_connection().get(link)
+                soup = BeautifulSoup(r.content, "html.parser")
 
-            self.parse_lyrics(soup, output, date)
+                output = self.parse_lyrics(soup, output, date)
+            iteration += 1
 
-        song_info_df = pd.DataFrame(output)
-        song_info_df = song_info_df.explode(['ranks',
-                                             'song_titles',
-                                             'artists',
-                                             'lyrics_links',
-                                             'lyrics']).reset_index(drop=True)
-        song_info_df = song_info_df[song_info_df.lyrics != 'We already listened to this one']
+        except Exception:
+            self.save_config(iteration)
+            output.to_csv('filename.csv')
+        song_info_df = output[output.lyrics != 'We already listened to this one']
         return song_info_df
 
-
 start_date = datetime(2019, 6, 1)
-end_date = datetime(2019, 6, 10)
-scraper = GeniusScraper(start_date, end_date)
+end_date = datetime(2019, 6, 5)
+scraper = GeniusScraper('./config.json', start_date, end_date)
 song_info_df = scraper.collect_data()
